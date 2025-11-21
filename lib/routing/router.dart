4c7aa47575718349521c999370d2521d5f19578f
treppenwitz03 <owenlegrande@gmail.com>
2025-11-21 11:48:54 +0800

    Merge

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -165,14 +165,6 @@ Future<String?> _redirect(BuildContext context, GoRouterState state) async {
 
     return Routes.login;
   }
-
-  if (state.matchedLocation == '/') {
-    if (loggedIn) {
-      return Routes.dashboard;
-    }
-
-    return Routes.login;
-  }
   
   // 1. Not logged in → allow reset flow
   if (!loggedIn) {
