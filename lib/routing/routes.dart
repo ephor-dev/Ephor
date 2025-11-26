04b81de1408efc744bf931ae4b928d0248fa8318
Addi M. <126937553+thoughtentity88@users.noreply.github.com>
2025-11-26 10:47:08 +0800

    Update routes.dart

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,24 +1,26 @@
 abstract final class Routes {
-  static const login = '/login';
-  static const dashboard = '/dashboard';
-  static const forgotPassword = '/forgot-password';
-  static const updatePassword = '/update-password';
+  static const login = '/login';
+  static const dashboard = '/dashboard';
+  static const forgotPassword = '/forgot-password';
+  static const updatePassword = '/update-password';
 
-  static const dashboardOverview = 'overview';
-  static const dashboardSchedules = 'schedules';
-  static const dashboardAssessments = 'assessments';
-  static const dashboardFinishedTrainings = 'finished-trainings';
-  static const dashboardRecommendedTrainings = 'recommended-trainings';
-  static const dashboardEmployeeList = 'employee-list';
-  static const dashboardAddEmployee = 'add';
-  static const dashboardEditEmployee = 'edit';
+  static const dashboardOverview = 'overview';
+  static const dashboardSchedules = 'schedules';
+  static const dashboardAssessments = 'assessments';
+  static const dashboardFinishedTrainings = 'finished-trainings';
+  static const dashboardRecommendedTrainings = 'recommended-trainings';
+  static const dashboardEmployeeList = 'employee-list';
+  static const dashboardAddEmployee = 'add';
+  static const dashboardEditEmployee = 'edit';
+  static const dashboardCatnaCreator = 'form-creator'; 
 
-  static String getOverviewPath() => '$dashboard/$dashboardOverview';
-  static String getSchedulesPath() => '$dashboard/$dashboardSchedules';
-  static String getAssessmentsPath() => '$dashboard/$dashboardAssessments';
-  static String getFinishedTrainingsPath() => '$dashboard/$dashboardFinishedTrainings';
-  static String getRecommendedTrainingsPath() => '$dashboard/$dashboardRecommendedTrainings';
-  static String getEmployeeListPath() => '$dashboard/$dashboardEmployeeList';
-  static String getAddEmployeePath() => '$dashboard/$dashboardEmployeeList/$dashboardAddEmployee';
-  static String getEditEmployeePath() => '$dashboard/$dashboardEmployeeList/$dashboardEditEmployee';
+  static String getOverviewPath() => '$dashboard/$dashboardOverview';
+  static String getSchedulesPath() => '$dashboard/$dashboardSchedules';
+  static String getAssessmentsPath() => '$dashboard/$dashboardAssessments';
+  static String getFinishedTrainingsPath() => '$dashboard/$dashboardFinishedTrainings';
+  static String getRecommendedTrainingsPath() => '$dashboard/$dashboardRecommendedTrainings';
+  static String getEmployeeListPath() => '$dashboard/$dashboardEmployeeList';
+  static String getAddEmployeePath() => '$dashboard/$dashboardEmployeeList/$dashboardAddEmployee';
+  static String getEditEmployeePath() => '$dashboard/$dashboardEmployeeList/$dashboardEditEmployee';
+  static String getCatnaCreatorPath() => '$dashboard/$dashboardCatnaCreator';
 }
